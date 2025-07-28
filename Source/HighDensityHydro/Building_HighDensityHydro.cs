18d85b7597e3bc30c7f60f5081cd41b34bd1bdb0
Darren Cheng <zadessc@gmail.com>
2025-07-27 19:33:41 -0700

    changed plant render to match vanilla, also changed the progress bar rendering on the 1x4 to look nicer

--- Source/HighDensityHydro/Building_HighDensityHydro.cs
+++ Source/HighDensityHydro/Building_HighDensityHydro.cs
@@ -90,10 +90,15 @@ namespace HighDensityHydro
 			this.LoadConfig();
 			int x = this.def.size.x;
 			float y = (x == 1) ? 0.6f : 0.1f;
+			y = 0.1f;
 			this.margin = ((x == 1) ? 0.15f : 0.08f);
+			this.margin = 0.05f;
 			this.barsize = new Vector2((float)this.def.size.z - 0.4f, y);
 
 			drawMatrixCache.Clear();
+			
+			//TODO: maybe move this somewhere else, should only be called and generated once
+			PlantPosIndices();
 		}
 
 		// Token: 0x0600000E RID: 14
@@ -128,12 +133,30 @@ namespace HighDensityHydro
 		{
 		    base.DrawAt(drawLoc, flip);
 			
-		    // Draw growth bar during Growing stage
+		    //Draw growth bar during Growing stage
 		    if (this.storedPlants > 0 && this.bayStage == BayStage.Growing)
 		    {
+			    Vector3 center;
+			    Vector2 offset;
+			    int x = this.def.size.x;
+			    
+			    //special position for the 1x4 hydroponics
+			    //height (y) of +0.01f above build, +0.02f are the plants 
+			    if (x == 1)
+			    {
+				    offset = Vector2.down * 0.3f;
+				    //center = drawLoc + Vector3.up * 0.01f + Vector3.forward * 0.3f;
+				    center = drawLoc + Vector3.up * 0.01f;
+			    }
+			    else
+			    {
+				    offset = Vector2.zero;
+				    center = drawLoc + Vector3.up * 0.01f;
+			    }
 		        GenDraw.FillableBarRequest barReq = new GenDraw.FillableBarRequest
 		        {
-		            center = drawLoc + Vector3.up * 0.1f,
+			        preRotationOffset = offset,
+		            center = center,
 		            size = this.barsize,
 		            fillPercent = this.growth,
 		            filledMat = HDH_Graphics.HDHBarFilledMat,
@@ -143,65 +166,245 @@ namespace HighDensityHydro
 		        };
 		        GenDraw.DrawFillableBar(barReq);
 		    }
+		    
+		    
+		    //
+		    // if (this.currentPlantDefToGrow == null || this.storedPlants <= 0)
+		    //     return;
+		    //
+		    // // Cache the graphic
+		    // Graphic graphic;
+		    // if (!plantGraphicCache.TryGetValue(this.currentPlantDefToGrow, out graphic))
+		    // {
+		    //     graphic = this.currentPlantDefToGrow.graphicData.Graphic;
+		    //     plantGraphicCache[this.currentPlantDefToGrow] = graphic;
+		    // }
+		    //
+		    // // Stable non-wind material
+		    // Material mat = new Material(graphic.MatSingle);
+		    // mat.shader = ShaderDatabase.Cutout;
+		    //
+		    // // Use in-game ticks
+		    // int ticks = Find.TickManager.TicksGame;
+		    // float swayAmplitude = 0.005f;
+		    // float swaySpeed = 0.1f; // radians per tick (adjust to taste)
+		    // //float scale = Mathf.Lerp(0.2f, 1.0f, this.growth);
+		    //
+		    // List<IntVec3> cells = this.OccupiedRect().Cells.ToList();
+		    // int cellCount = cells.Count;
+		    //
+		    // int visibleCount = 0;
+		    // if (this.bayStage == BayStage.Growing)
+		    // {
+			   //  visibleCount = cellCount;
+		    // }
+		    // else if (this.bayStage == BayStage.Sowing && this.storedPlants > 0)
+		    // {
+			   //  visibleCount = Mathf.Clamp(Mathf.FloorToInt((this.storedPlants / (float)this.capacity) * cellCount), 1, cellCount);
+		    // }
+		    // else if (this.bayStage == BayStage.Harvest)
+		    // {
+			   //  visibleCount = 0;
+		    // }
+		    //
+		    // for (int i = 0; i < visibleCount; i++)
+		    // {
+			   //  IntVec3 cell = cells[i];
+		    //
+			   //  float phaseOffset = (cell.x * 17 + cell.z * 31) % 100 / 100f;
+			   //  float swayOffset = Mathf.Sin(ticks * swaySpeed + phaseOffset * Mathf.PI * 2f) * swayAmplitude;
+		    //
+			   //  Vector3 cellDrawPos = cell.ToVector3ShiftedWithAltitude(AltitudeLayer.BuildingOnTop);
+			   //  cellDrawPos.x += swayOffset;
+		    //
+			   //  float scale = Mathf.Lerp(0.2f, 1.0f, this.growth);
+		    //
+			   //  Matrix4x4 matrix = Matrix4x4.TRS(
+				  //   cellDrawPos,
+				  //   Quaternion.identity,
+				  //   new Vector3(scale, 1f, scale)
+			   //  );
+		    //
+			   //  Graphics.DrawMesh(MeshPool.plane10, matrix, mat, 0);
+		    // }
+		}
+		
+		//draw plants stuff
+		private static readonly Color32[] workingColors = new Color32[4]; // mimic Plant.workingColors
 
-		    if (this.currentPlantDefToGrow == null || this.storedPlants <= 0)
-		        return;
+		//draw plants stuff
+		//TODO: clean up this code
+		public override void Print(SectionLayer layer)
+		{
+			base.Print(layer);
+			
+			if (this.currentPlantDefToGrow == null || this.storedPlants <= 0)
+				return;
+			
+			List<IntVec3> cells = this.OccupiedRect().Cells.ToList();
+			int cellCount = cells.Count;
+			int visibleCount = 0;
+			if (bayStage == BayStage.Growing)
+			{
+				visibleCount = cellCount;
+			}
+			else if (this.bayStage == BayStage.Sowing && this.storedPlants > 0)
+			{
+				visibleCount = Mathf.Clamp(Mathf.FloorToInt((this.storedPlants / (float)this.capacity) * cellCount), 1, cellCount);
+			}
+			else if (this.bayStage == BayStage.Harvest)
+			{
+				visibleCount = 0;
+			}
+			
+			Graphic graphic = currentPlantDefToGrow.graphicData.Graphic;
+			if (growth <= currentPlantDefToGrow.plant.harvestMinGrowth && currentPlantDefToGrow.plant.immatureGraphic != null)
+			{
+				graphic = currentPlantDefToGrow.plant.immatureGraphic;
+			}
 
-		    // Cache the graphic
-		    Graphic graphic;
-		    if (!plantGraphicCache.TryGetValue(this.currentPlantDefToGrow, out graphic))
-		    {
-		        graphic = this.currentPlantDefToGrow.graphicData.Graphic;
-		        plantGraphicCache[this.currentPlantDefToGrow] = graphic;
-		    }
+			Material baseMat = graphic.MatSingleFor(this);
+			//Material mat = graphic.MatAt(Rot4.North); ;
 
-		    // Stable non-wind material
-		    Material mat = new Material(graphic.MatSingle);
-		    mat.shader = ShaderDatabase.Cutout;
+			for (int i = 0; i < visibleCount; i++)
+			{
+				Vector3 center = cells[i].ToVector3ShiftedWithAltitude(AltitudeLayer.BuildingOnTop) + Vector3.up * 0.02f;
+				Rand.PushState();
+				Rand.Seed = Gen.HashCombineInt(this.Position.GetHashCode(), i);
 
-		    // Use in-game ticks
-		    int ticks = Find.TickManager.TicksGame;
-		    float swayAmplitude = 0.005f;
-		    float swaySpeed = 0.1f; // radians per tick (adjust to taste)
-		    //float scale = Mathf.Lerp(0.2f, 1.0f, this.growth);
+				int num = Mathf.CeilToInt(this.growth * (float)currentPlantDefToGrow.plant.maxMeshCount);
+				if (num < 1) num = 1;
+				
+				float num2 = this.currentPlantDefToGrow.plant.visualSizeRange.LerpThroughRange(this.growth);
+				float num3 = this.currentPlantDefToGrow.graphicData.drawSize.x * num2;
+				int num4 = 0;
+				int[] positionIndices = GetStablePositionIndices(currentPlantDefToGrow, center, i);
+				bool flag = false;
+				foreach (int num5 in positionIndices)
+				{
+					Vector3 vector;
+					if (currentPlantDefToGrow.plant.maxMeshCount != 1)
+					{
+						int num6 = 1;
+						int maxMeshCount = currentPlantDefToGrow.plant.maxMeshCount;
+						switch (maxMeshCount)
+						{
+							case 1:  num6 = 1; break;
+							case 4:  num6 = 2; break;
+							case 9:  num6 = 3; break;
+							case 16: num6 = 4; break;
+							case 25: num6 = 5; break;
+							default:
+								//throw new ArgumentException($"{def} must have plant.maxMeshCount that is a perfect square.");
+								Log.Error($"[HDH] {def} must have plant.maxMeshCount that is a perfect square.");
+								break;
+						}
+						float num7 = 1f / (float)num6;
 
-		    List<IntVec3> cells = this.OccupiedRect().Cells.ToList();
-		    int cellCount = cells.Count;
+						vector = center;
+						vector.y = this.def.Altitude;
 
-		    int visibleCount = 0;
-		    if (this.bayStage == BayStage.Growing)
-		    {
-			    visibleCount = cellCount;
-		    }
-		    else if (this.bayStage == BayStage.Sowing && this.storedPlants > 0)
-		    {
-			    visibleCount = Mathf.Clamp(Mathf.FloorToInt((this.storedPlants / (float)this.capacity) * cellCount), 1, cellCount);
-		    }
-		    else if (this.bayStage == BayStage.Harvest)
-		    {
-			    visibleCount = 0;
-		    }
+						int num8 = num5 / num6;
+						int num9 = num5 % num6;
 
-		    for (int i = 0; i < visibleCount; i++)
-		    {
-			    IntVec3 cell = cells[i];
+						float spacing = 1f / (float)num6;
+						vector.x += (num8 - (num6 - 1) * 0.5f) * spacing;
+						vector.z += (num9 - (num6 - 1) * 0.5f) * spacing;
+						
+						float max = num7 * 0.3f;
+						vector += Gen.RandomHorizontalVector(max);
+					}
+					else
+					{
+						vector = center + Gen.RandomHorizontalVector(0.05f);
+						float num10 = (float)center.z -0.5f;
+						if (vector.z - num2 / 2f < num10)
+						{
+							vector.z = num10 + num2 / 2f + 0.2f;
+							flag = true;
+						}
+					}
+					
+					bool @bool = Rand.Bool;
 
-			    float phaseOffset = (cell.x * 17 + cell.z * 31) % 100 / 100f;
-			    float swayOffset = Mathf.Sin(ticks * swaySpeed + phaseOffset * Mathf.PI * 2f) * swayAmplitude;
+					// if (Graphic is Graphic_Random)
+					// {
+					// 	mat = graphic.MatSingleFor(this);
+					// }
 
-			    Vector3 cellDrawPos = cell.ToVector3ShiftedWithAltitude(AltitudeLayer.BuildingOnTop);
-			    cellDrawPos.x += swayOffset;
+					Material mat = baseMat;
+					
+					Vector2[] uvs;
+					Color32 color;
+					Graphic.TryGetTextureAtlasReplacementInfo(mat, ThingCategory.Plant.ToAtlasGroup(), @bool, false, out mat, out uvs, out color);
+					SetWindExposureColors(workingColors, currentPlantDefToGrow);
+					Vector2 size = new Vector2(num3, num3);
+					Printer_Plane.PrintPlane(layer, vector, size, mat, 0f, @bool, uvs, workingColors, 0.1f, (float)(this.HashOffset() % 1024));
+					num4++;
+					if (num4 >= num)
+					{
+						break;
+					}
+					
+				}
+				if (this.def.graphicData.shadowData != null)
+				{
+					Vector3 center2 = center + this.def.graphicData.shadowData.offset * num2;
+					if (flag)
+					{
+						center2.z = base.Position.ToVector3Shifted().z + this.def.graphicData.shadowData.offset.z;
+					}
+					center2.y -= 0.03658537f;
+					Vector3 volume = this.def.graphicData.shadowData.volume * num2;
+					Printer_Shadow.PrintShadow(layer, center, volume, Rot4.North);
+				}
+				Rand.PopState();
+				
+			}
+			
+		}
+		
+		//draw plants stuff
+		public static void SetWindExposureColors(Color32[] colors, ThingDef plant)
+		{
+			colors[1].a = (colors[2].a = GetWindExposure(plant));
+			colors[0].a = (colors[3].a = 0);
+		}
+		
+		//draw plants stuff
+		public static byte GetWindExposure(ThingDef plant)
+		{
+			return (byte)Mathf.Min(255f * plant.plant.topWindExposure, 255f);
+		}
+		
+		//draw plants stuff
+		private static int[][][] rootList = new int[25][][];
+		//draw plants stuff
+		static void PlantPosIndices()
+		{
+			for (int i = 0; i < 25; i++)
+			{
+				rootList[i] = new int[8][];
+				for (int j = 0; j < 8; j++)
+				{
+					int[] array = new int[i + 1];
+					for (int k = 0; k < i; k++)
+					{
+						array[k] = k;
+					}
 
-			    float scale = Mathf.Lerp(0.2f, 1.0f, this.growth);
-
-			    Matrix4x4 matrix = Matrix4x4.TRS(
-				    cellDrawPos,
-				    Quaternion.identity,
-				    new Vector3(scale, 1f, scale)
-			    );
-
-			    Graphics.DrawMesh(MeshPool.plane10, matrix, mat, 0);
-		    }
+					array.Shuffle<int>();
+					rootList[i][j] = array;
+				}
+			}
+		}
+		
+		//draw plants stuff
+		private static int[] GetStablePositionIndices(ThingDef plant, Vector3 cell, int index)
+		{
+			int maxMeshCount = plant.plant.maxMeshCount;
+			int num = ((cell.GetHashCode() + index) ^ 42348528) % 8;
+			return rootList[maxMeshCount - 1][num];
 		}
 
 		// Token: 0x0600012D RID: 301
@@ -426,10 +629,10 @@ namespace HighDensityHydro
 		private float margin;
 
 		// Token: 0x040000CD RID: 205
-		private int storedPlants;
+		private int storedPlants = 0;
 
 		// Token: 0x040000CE RID: 206
-		private float growth;
+		private float growth = 0;
 
 		// Token: 0x040000D1 RID: 209
 		private CompPowerTrader powerCompCached;
